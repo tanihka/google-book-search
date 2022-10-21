29ca1b9f8f7c808ba5e46249527655a87805aa1e
tanishkaa <taanishkag078@gmail.com>
2022-10-21 15:03:07 +0530

    Updates

---
+++ precache-manifest.37c8d09e16d87e95762a68afcda6baae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360103c373b208baee01f877b0a7bd7f",
+    "url": "/google-book-search/index.html"
+  },
+  {
+    "revision": "42e211a68886872d28f4",
+    "url": "/google-book-search/static/css/2.a8ba0f7c.chunk.css"
+  },
+  {
+    "revision": "969a03e05c5c586bc8eb",
+    "url": "/google-book-search/static/css/main.2331b85f.chunk.css"
+  },
+  {
+    "revision": "42e211a68886872d28f4",
+    "url": "/google-book-search/static/js/2.857712a8.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/google-book-search/static/js/2.857712a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969a03e05c5c586bc8eb",
+    "url": "/google-book-search/static/js/main.cdb1659a.chunk.js"
+  },
+  {
+    "revision": "8c3fee29a919ec650f34",
+    "url": "/google-book-search/static/js/runtime-main.d3ff930f.js"
+  }
+]);
